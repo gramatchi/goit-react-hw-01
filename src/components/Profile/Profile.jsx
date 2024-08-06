c7748caac8ca3a42ad3e7a9f2ba24b5b8aff2ddd
gramatchi <nik98.champion@gmail.com>
2024-08-06 21:47:02 +0200

    style profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,27 +1,30 @@
-const Profile = ({ name, tag, location, image, stats }) => {  
-    return (
-      <div>
-        <div>
-          <img
-            src={image}
-            alt="User avatar"
-          />
-          <p>{name}</p>
-          <p>@{tag}</p>
-          <p>{location}</p>
-        </div>
-  
-        <ul>
-          {Object.entries(stats).map(([key, value]) => (
-            <li key={key}>
-              <span>{key}</span>
-              <span>{value}</span>
-            </li>
-          ))}
-        </ul>
+import s from "./Profile.module.css";
+
+const Profile = ({ name, tag, location, image, stats }) => {
+  return (
+    <div className={s.card}>
+      <div className={s.image}>
+        <img src={image} alt="User avatar" className={s.icon} />
+        <p className={(s.info, s.name)}>{name}</p>
+        <p className={s.info}>@{tag}</p>
+        <p className={s.info}>{location}</p>
       </div>
-    );
-  };
-  
-  export default Profile;
-  
+
+      <ul className={s.list}>
+        {Object.entries(stats).map(([key, value]) => (
+          <li className={s.item} key={key}>
+            <span>
+              {key
+                .split(" ")
+                .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                .join(" ")}
+            </span>
+            <span className={s.value}>{value}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
